1eeab1ceb28097a71ce8aaaa10b76ed8c9678258
Carolina Pineda <gpinedacarolina@gmail.com>
2023-08-11 19:33:21 -0600

    Creacion de mi seeder

--- .sequelizerc.mjs
+++ .sequelizerc.mjs
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 
 export default {
   config: resolve('config', 'config.js'),     // Ruta a tu archivo de configuración de Sequelize
-//   'models-path': resolve('src', 'models'),    // Ruta a tus modelos
-  'seeders-path': resolve('src', 'seeders'),  // Ruta a tus seeders
-//   'migrations-path': resolve('src', 'migrations')  // Ruta a tus migraciones
+  'models-path': resolve( 'models'),    // Ruta a tus modelos
+  'seeders-path': resolve('src'),  // Ruta a tus seeders
+  'migrations-path': resolve('migrations')  // Ruta a tus migraciones
 };
