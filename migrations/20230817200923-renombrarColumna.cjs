ee7699525565c72162e74a60276e977dfb48d5b7
Carolina Pineda Grifaldo <gpinedacarolina@gmail.com>
2023-08-18 15:15:31 -0600

    add

--- migrations/20230817200923-renombrarColumna.cjs
+++ migrations/20230817200923-renombrarColumna.cjs
@@ -3,10 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.renameColumn('Categorias', 'nombre', 'categoria');
+    await queryInterface.renameColumn();
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.renameColumn('Categorias', 'nombre', 'categoria');
+    await queryInterface.renameColumn();
   }
 };
